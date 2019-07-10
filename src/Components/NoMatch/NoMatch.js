ecb6193329d91e820dffe5c432c95b1ea59a924b
raechel <odom.raechel@gmail.com>
2019-07-10 10:01:49 -0600

    404 image updated to sprinkleboy

--- src/Components/NoMatch/NoMatch.js
+++ src/Components/NoMatch/NoMatch.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import image from '../../assets/sprinkleboy.jpeg';
 
 export default function NoMatch() {
   return (
     <section className="NoMatch">
-      <h3>Sorry! That page doesn't exist.</h3>
-      <Link className="back-btn" to='/'>
-        <button>Go Back</button>
-      </Link>
+      <section className="no-match-link">
+        <h3>Sorry! That page doesn't exist.</h3>
+        <Link className="back-btn" to='/'>
+          <button>Go Back</button>
+        </Link>
+      </section>
+      <img src={image} alt="boys face covered in sprinkles" />
     </section>
   )
 }
