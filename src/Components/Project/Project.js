8679eb94eecf7151ba83a418252dbed56c67fcda
raechel <odom.raechel@gmail.com>
2019-07-10 15:33:42 -0600

    project popup add and styles began

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ReactModal from 'react-modal';
 
-export default function Project({name}) {
+class Project extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal: false
+    }
+  }
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  }
+  
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  }
+
+ render() {
+
   return (
-    <article className="Project">
-      <h4 className="Project-name">{name}</h4>
-      <i className="fas fa-pencil-alt project-icon"></i>
-      <i className="fas fa-trash-alt project-icon"></i>
-    </article>
+    <section>
+      <article className="Project">
+        <h4 className="Project-name">{this.props.name}</h4>
+        <i onClick={this.handleOpenModal} className="fas fa-pencil-alt project-icon"></i>
+        <i className="fas fa-trash-alt project-icon"></i>
+      </article>
+      <ReactModal 
+           isOpen={this.state.showModal}
+           contentLabel="Palette Modal"
+        >
+          <button onClick={this.handleCloseModal}>Close</button>
+        </ReactModal>
+    </section>
   )
+ }
 }
 
+export default Project;
