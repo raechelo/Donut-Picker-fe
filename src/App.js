e2aa69efa59243194e47e47fa6800d5a9d9d7942
Rachael Drennan <rdrennan0@gmail.com>
2019-07-10 20:22:25 -0600

    file structure conflict

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./App.css";
+import Donut from "./Donut";
+const shortid = require('shortid');
+ 
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      donuts: [1,2,3,4,5,6]
+    };
+  }
+
+
+  componentDidMount = () => {
+  };
+
+
+  render() {
+    let donuts = this.state.donuts.map(donut => {
+      return <Donut key= {shortid.generate()} />
+
+    })
+    return (
+      <div className="App">
+      {donuts}
+      </div>
+    );
+  }
+}
+
+export default App;
