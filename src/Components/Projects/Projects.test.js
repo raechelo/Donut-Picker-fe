47d0dd169c8a624e8e4263b2b8e722673b04a6fd
Rachael Drennan <rdrennan0@gmail.com>
2019-07-11 08:14:45 -0600

    Home and Projects test

--- src/Components/Projects/Projects.test.js
+++ src/Components/Projects/Projects.test.js
@@ -5,6 +5,13 @@ import Projects from './Projects'
 
 describe('Projects', () => {
     let wrapper;
+    window.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve()
+    })
+  );
     let mockProjects = [{name: "Testing", key: 1}, {name: "Also Testing", key: 2}]
     wrapper = shallow( <Projects projects={mockProjects}/> )
     it('should matchsnapshot', () => {
@@ -14,7 +21,11 @@ describe('Projects', () => {
         wrapper.setState({ name: 'test' });
         wrapper.instance().handleChange({target:{value:"diff"}})
         expect(wrapper.state('name')).toEqual("diff")
-
     });
+    it("should call the correct URL when fetching palettes on postProject()", () => {
+        let URL = "http://localhost:3001/api/v1/projects";
+        wrapper.instance().postProject();
+        expect(window.fetch).toHaveBeenCalledWith(URL,{"body": "{\"name\":\"diff\"}", "headers": {"Accept": "application/json", "Content-Type": "application/json"}, "method": "POST"})
+      });
     
 });
